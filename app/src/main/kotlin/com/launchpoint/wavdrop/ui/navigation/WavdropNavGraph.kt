419bc39e116d18294a1b8c963702d1814b3a80f1
Walter Mafu <hello.launchpointdigital@gmail.com>
2026-06-20 16:58:13 +0200

    Align Home insights shortcut with primary navigation

--- app/src/main/kotlin/com/launchpoint/wavdrop/ui/navigation/WavdropNavGraph.kt
+++ app/src/main/kotlin/com/launchpoint/wavdrop/ui/navigation/WavdropNavGraph.kt
@@ -223,7 +223,7 @@ fun WavdropNavGraph(
                 onAlbumClick             = { albumKey -> navController.navigate(Screen.AlbumDetails.createRoute(albumKey)) },
                 onArtistClick            = { artistKey -> navController.navigate(Screen.ArtistDetails.createRoute(artistKey)) },
                 onGlobalSearchClick      = { navController.navigate(Screen.GlobalSearch.route) },
-                onReportsAndInsightsClick = { navController.navigate(Screen.SettingsStatistics.route) },
+                onReportsAndInsightsClick = { navController.navigatePrimary(Screen.SettingsStatistics.route) },
             )
         }
         composable(Screen.Library.route) {
