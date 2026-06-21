7f28d05423059a85b89c8c78a0ee2bdf4b5386c6
Walter Mafu <hello.launchpointdigital@gmail.com>
2026-06-21 02:30:13 +0200

    Add Smart Collections to global search

--- app/src/main/kotlin/com/launchpoint/wavdrop/ui/screen/search/GlobalSearchViewModel.kt
+++ app/src/main/kotlin/com/launchpoint/wavdrop/ui/screen/search/GlobalSearchViewModel.kt
@@ -17,7 +17,6 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -37,7 +36,6 @@ class GlobalSearchViewModel @Inject constructor(
     fun setSearchQuery(query: String) { _searchQuery.value = query }
 
     val allSongs: StateFlow<List<Song>> = songRepository.songs
-        .map { it }
         .stateIn(
             scope        = viewModelScope,
             started      = SharingStarted.WhileSubscribed(5_000),
